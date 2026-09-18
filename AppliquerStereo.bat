7f68cbe298edcfd6a4ab744063349892ff2439c9
Franck Fornasari <86000279+Kraran@users.noreply.github.com>
2026-09-18 22:27:32 +0200

    chore: remove AppliquerStereo.bat

--- AppliquerStereo.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Applying stereo pan to this install...
-python inject_pan.py
-if errorlevel 1 py inject_pan.py
-echo.
-echo You can close this window and launch the game.
-pause
